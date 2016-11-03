5edbf7bb162eee4cd313de03b2a6c3072b80980f
novicasarenac <nolesarenac@yahoo.com>
2016-11-03 18:30:04 +0100

    Project initialization

---
+++ ISA/src/main/java/ISAProject/Isa2016Application.java
@@ -0,0 +1,13 @@
+package ISAProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Isa2016Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Isa2016Application.class, args);
+		System.out.println("Poyy");
+	}
+}
