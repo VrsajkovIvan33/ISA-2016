1b6b00ee05a65dc51c92367ab06f78c344f84cb5
novicasarenac <nolesarenac@yahoo.com>
2016-11-07 20:32:13 +0100

    welcome page

--- ISA/src/main/java/ISAProject/Isa2016Application.java
+++ ISA/src/main/java/ISAProject/Isa2016Application.java
@@ -8,6 +8,5 @@ public class Isa2016Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Isa2016Application.class, args);
-		System.out.println("Poyy");
 	}
 }
---
+++ ISA/src/main/java/ISAProject/controller/WelcomeController.java
@@ -0,0 +1,18 @@
+package ISAProject.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Nole on 11/7/2016.
+ */
+@RestController
+@RequestMapping(value = "/")
+public class WelcomeController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String welcome(){
+        return "o sta ima";
+    }
+}
