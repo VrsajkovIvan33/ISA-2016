b8277cb8a56b3b2c1ca756987ef2ab0a46b017bb
VrsajkovIvan33 <vrsajkov.ivan@gmail.com>
2016-11-24 23:20:47 +0100

    Initial angular register controller add

---
+++ ISA/src/main/resources/static/js/controllers/guestRegisterController.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Verpsychoff on 11/24/2016.
+ */
+
+angular.module('restaurantApp.GuestRegisterController',[])
+    .controller('GuestRegisterController', function ($scope) {
+        function init() {
+            console.log("Kao neko registrovanje");
+        }
+
+        init();
+
+    });
