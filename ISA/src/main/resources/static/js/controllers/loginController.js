1e3eab9fb3ab77553a2fd10d7d5120ffe0406da9
novicasarenac <nolesarenac@yahoo.com>
2016-11-24 23:00:16 +0100

    Structure of frontend made

---
+++ ISA/src/main/resources/static/js/controllers/loginController.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Nole on 11/24/2016.
+ */
+
+angular.module('restaurantApp.LoginController',[])
+       .controller('LoginController', function ($scope) {
+           function init() {
+               console.log("logovo se");
+           }
+
+           init();
+
+       });
--- ISA/src/main/resources/static/js/modules/loginModule.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by Nole on 11/23/2016.
- */
-
-var loginModule = angular.module('loginModule', []);
