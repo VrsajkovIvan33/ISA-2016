6ca3203df2af63104aa3901db0feb83c75eba0b2
VrsajkovIvan33 <vrsajkov.ivan@gmail.com>
2016-11-25 02:06:48 +0100

    Implement guest registration

--- ISA/src/main/resources/static/js/controllers/guestRegisterController.js
+++ ISA/src/main/resources/static/js/controllers/guestRegisterController.js
@@ -3,11 +3,22 @@
  */
 
 angular.module('restaurantApp.GuestRegisterController',[])
-    .controller('GuestRegisterController', function ($scope) {
+    .controller('GuestRegisterController', function ($scope, GuestRegisterFactory) {
         function init() {
             console.log("Kao neko registrovanje");
+            $scope.newGuest = new Object();
+            $scope.newGuest.gName = "";
+            $scope.newGuest.gSurname = "";
+            $scope.newGuest.gPassword = "";
+            $scope.newGuest.gEmail = "";
         }
 
         init();
+
+        $scope.registerGuest = function() {
+            GuestRegisterFactory.postGuest($scope.newGuest).success(function(data) {
+                console.log("Pokusao da registruje!")
+            });
+        }
 
     });
---
+++ ISA/src/main/resources/static/js/services/guestRegisterFactory.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Verpsychoff on 11/25/2016.
+ */
+angular.module('restaurantApp.GuestRegisterFactory',[])
+    .factory('GuestRegisterFactory', function ($http) {
+
+        var factory = {};
+
+        factory.postGuest = function(newGuest) {
+            return $http.post('/registerGuest', newGuest)
+        }
+
+        return factory;
+
+    });
