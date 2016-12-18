b5015283f32e123783806b7aed2de041cc6d31bf
VrsajkovIvan33 <vrsajkov.ivan@gmail.com>
2016-12-18 20:05:11 +0100

    Add bower and the ui-calendar

---
+++ ISA/src/main/resources/static/js/controllers/waiterController.js
@@ -0,0 +1,61 @@
+/**
+ * Created by Verpsychoff on 12/18/2016.
+ */
+
+angular.module('restaurantApp.WaiterController',[])
+    .controller('WaiterController', function ($scope) {
+        function init() {
+            console.log("Kao neko registrovanje");
+            var date = new Date();
+            var d = date.getDate();
+            var m = date.getMonth();
+            var y = date.getFullYear();
+
+            $scope.myEvents = [
+                {
+                    title: 'All Day Test Event',
+                    start: new Date(y, m, 1)
+                },
+                {
+                    title: 'Long Test Event',
+                    start: new Date(y, m, d - 5),
+                    end: new Date(y, m, d - 2)
+                },
+                {
+                    title: 'Test Birthday Party',
+                    start: new Date(y, m, d + 1, 19, 0),
+                    end: new Date(y, m, d + 1, 22, 30),
+                    allDay: false
+                }
+            ];
+
+            $scope.eventSources = [$scope.myEvents];
+
+            $scope.alertOnEventClick = function( date, jsEvent, view){
+                console.log("Kliknut dogadjaj!");
+            };
+
+            $scope.uiConfig = {
+                calendar: {
+                    height: 450,
+                    editable: true,
+                    header: {
+                        left: 'month basicWeek basicDay agendaWeek agendaDay',
+                        center: 'title',
+                        right: 'today prev, next'
+                    },
+                    eventClick: $scope.alertOnEventClick
+                }
+            }
+
+            $scope.changeView = function(view,calendar) {
+                uiCalendarConfig.calendars[calendar].fullCalendar('changeView',view);
+            };
+
+        }
+
+        init();
+
+
+
+    });
