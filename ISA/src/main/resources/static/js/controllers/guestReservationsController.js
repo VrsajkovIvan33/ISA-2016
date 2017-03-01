c30e21e4eb2d633a0f6339e17c7b235f5fd39462
novicasarenac <nolesarenac@yahoo.com>
2017-03-01 01:25:55 +0100

    Order item delete fixed

--- ISA/src/main/resources/static/js/controllers/guestReservationsController.js
+++ ISA/src/main/resources/static/js/controllers/guestReservationsController.js
@@ -112,8 +112,11 @@ angular.module('restaurantApp.GuestReservationsController', [])
                        }
                    }
                }).result.then(function(orderItem){
-                   $scope.reservation.order.orderItems.push(orderItem);
+                   //$scope.reservation.order.orderItems.push(orderItem);
                    GuestReservationsFactory.updateReservation($scope.reservation.id, orderItem).success(function(data){
+                       GuestReservationsFactory.getReservation($scope.reservation.id).success(function (data) {
+                           $scope.reservation = data;
+                       })
                        toastr.success('Order added!');
                    });
                });
